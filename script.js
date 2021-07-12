e32451af154f6095aa77d96892c006caec3f1025
Andrzej Trufanow <andrzej3026@gmail.com>
2021-07-12 23:42:48 +0500

    wtf

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 const addTask = document.getElementById('button-add')
 const textInput = document.getElementById('input')
 const list = document.getElementById('list')
+const form = document.getElementById('form')
 
-addTask.addEventListener('submit', function(event) {
-    event.preventDefault()
+form.addEventListener('submit', function(event) {
+    event.preventDefault();
+    console.log(event);
     list.style.display = 'block';
     const li = document.createElement('li');
     li.innerHTML = textInput.value;
@@ -11,10 +13,12 @@ addTask.addEventListener('submit', function(event) {
     textInput.value = '';
 
     const remove = document.createElement('button')
-    remove.classList = 'button-remove'
-    remove.textContent = '-'
-    li.appendChild(remove)
-    remove.addEventListener('click', function() {
-        list.removeChild(li)
+    remove.classList = 'button-remove';
+    remove.textContent = '-';
+    li.appendChild(remove);
+    remove.addEventListener('click', function(event) {
+        console.log(event);
+        // list.removeChild(li);
+        // console.log(event.target.innerHTML);
     })
 })
