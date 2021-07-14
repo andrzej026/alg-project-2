ffda56674a72ffeb5c91c4b3d250194c2f52fdd1
Andrzej Trufanow <andrzej3026@gmail.com>
2021-07-15 00:13:59 +0500

    sort-1

--- script.js
+++ script.js
@@ -1,24 +1,36 @@
-const addTask = document.getElementById('button-add')
-const textInput = document.getElementById('input')
-const list = document.getElementById('list')
-const form = document.getElementById('form')
+const addTask = document.querySelector('#button-add')
+const textInput = document.querySelector('#input')
+const list = document.querySelector('#list')
+const form = document.querySelector('#form')
 
 form.addEventListener('submit', function(event) {
     event.preventDefault();
-    console.log(event);
     list.style.display = 'block';
     const li = document.createElement('li');
     li.innerHTML = textInput.value;
     list.appendChild(li);
     textInput.value = '';
 
-    const remove = document.createElement('button')
+    const remove = document.createElement('span')
     remove.classList = 'button-remove';
     remove.textContent = '-';
     li.appendChild(remove);
     remove.addEventListener('click', function(event) {
-        console.log(event);
-        // list.removeChild(li);
-        // console.log(event.target.innerHTML);
+        list.removeChild(li);
+
     })
+    const sorting = document.querySelector('.sorting')
+    const items = document.querySelectorAll('#list li');
+    sorting.addEventListener('click', sortedElements())
+
+    function sortedElements() {
+        let sorted = [...items].sort(function(a, b) {
+            if (a.innerHTML > b.innerHTML) {return 1;}
+            if (a.innerHTML < b.innerHTML) {return -1;}
+        });
+        list.innerHTML = '';
+        for (let li of sorted) {
+            list.appendChild(li);
+        }
+    }
 })
