587ac77ab3b0a0fc524b91ef4bcf8266913ec56d
Andrzej Trufanow <andrzej3026@gmail.com>
2021-07-22 23:00:00 +0500

    sort-problem

--- script.js
+++ script.js
@@ -2,35 +2,52 @@ const addTask = document.querySelector('#button-add')
 const textInput = document.querySelector('#input')
 const list = document.querySelector('#list')
 const form = document.querySelector('#form')
+const sorting = document.querySelector('.sorting')
 
 form.addEventListener('submit', function(event) {
     event.preventDefault();
-    list.style.display = 'block';
-    const li = document.createElement('li');
-    li.innerHTML = textInput.value;
-    list.appendChild(li);
-    textInput.value = '';
+    if (textInput.value.trim() === '' || textInput.value === null) {
+        return false
+    } else {
+        list.style.border = '1px solid #C4C4C4';
+        let li = document.createElement('li');
+        let task = document.createElement('span')
+        task.classList.add('task')
+        task.innerHTML = textInput.value;
+        list.appendChild(li);
+        textInput.value = '';
+        task.setAttribute('contenteditable', 'true')
+        li.appendChild(task)
 
-    const remove = document.createElement('span')
-    remove.classList = 'button-remove';
-    remove.textContent = '-';
-    li.appendChild(remove);
-    remove.addEventListener('click', function(event) {
-        list.removeChild(li);
-
-    })
-    const sorting = document.querySelector('.sorting')
-    const items = document.querySelectorAll('#list li');
-    sorting.addEventListener('click', sortedElements())
-
-    function sortedElements() {
-        let sorted = [...items].sort(function(a, b) {
-            if (a.innerHTML > b.innerHTML) {return 1;}
-            if (a.innerHTML < b.innerHTML) {return -1;}
-        });
-        list.innerHTML = '';
-        for (let li of sorted) {
-            list.appendChild(li);
-        }
+        const remove = document.createElement('span')
+        remove.classList = 'button-remove';
+        remove.innerHTML = '&times';
+        li.appendChild(remove);
+        remove.addEventListener('click', function(event) {
+            list.removeChild(li);
+            // console.log(list);
+        })
+        
+        
+        const items = document.querySelectorAll('#list li');
+        let sorted = [...items]
+        // console.log(list);
+        // console.log(items);
+        sorting.addEventListener('click', function() {
+            list.innerHTML = '';
+            sorted.sort(function(a, b) {
+                console.log(items)
+                if (a.innerHTML > b.innerHTML) {return 1;}
+                if (a.innerHTML < b.innerHTML) {return -1;}
+            });
+            list.innerHTML = '';        
+            sorting.setAttribute('src', 'images/Group-90.svg')
+            sorting.classList.remove('down')
+            sorting.classList.add('up')
+            for (let task of sorted) {
+                list.appendChild(task);
+            }
+            
+        })
     }
 })
