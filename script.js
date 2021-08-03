11a15fc3b24541cdd20fc472c44fd48e1871bcdd
Andrzej Trufanow <andrzej3026@gmail.com>
2021-08-03 16:15:51 +0500

    smthng wrks

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-const addTask = document.querySelector('#button-add')
-const textInput = document.querySelector('#input')
-const list = document.querySelector('#list')
-const form = document.querySelector('#form')
-const sorting = document.querySelector('.sorting')
-
+const addTask = document.querySelector('#button-add');
+const textInput = document.querySelector('#input');
+const list = document.querySelector('#list');
+const form = document.querySelector('#form');
+const sorting = document.querySelector('.sorting');
+const items = list.childNodes;
 
 form.addEventListener('submit', function(event) {
     event.preventDefault();
@@ -12,58 +12,60 @@ form.addEventListener('submit', function(event) {
     } else {
         list.style.display = 'block';
         const item = document.createElement('li');
-        const task = document.createElement('span')
-        item.classList.add('item')
-        task.classList.add('task')
+        const task = document.createElement('span');
+        item.classList.add('item');
+        task.classList.add('task');
         task.innerHTML = textInput.value;
         task.setAttribute('contenteditable', 'true')
         list.appendChild(item);
-        item.appendChild(task)
-        textInput.value = '';
-
+        item.appendChild(task);
+        textInput.value = '';      
         
-        const items = document.querySelectorAll('.item')
         const remove = document.createElement('span')
         remove.classList = 'button-remove';
         remove.innerHTML = '&times';
         item.appendChild(remove);
         remove.addEventListener('click', function(event) {
-            
-            console.log(items.length);
             if (items.length === 1) {
-                // console.log(items);
                 list.removeChild(item);
-                // list.style.display = 'none';
+                list.style.display = 'none';
             } else {
-                console.log(item);
-                // console.log(items);
                 list.removeChild(item);
             }
-        })
-
-        
-        
-        // console.log(list);
-        // console.log(items);
-        sorting.addEventListener('click', function() {
-            let sorted = [];
-            for (let i = 0; i < items.length; i++) {
-                // console.log(items);
-                sorted.push(items[i]);
-            }
-            sorted.sort(function(a, b) {
-                // console.log(items);
-                if (a.textContent > b.textContent) {return 1;}
-                if (a.textContent < b.textContent) {return -1;}
-            });
-            sorted.forEach(task => list.appendChild(task));
-            sorting.setAttribute('src', 'images/Group-90.svg');
-            sorting.classList.replace('down', 'up');
-            console.log(sorted);
-            // list.innerHTML = ''
-            // for (let task of sorted) {
-            //     list.appendChild(task);
-            // } 
         })
     }
 })
+
+sorting.addEventListener('click', function () {
+    if (sorting.className === 'sorting down') {
+        sortingup();
+    } else if (sorting.className === 'sorting up'){
+        sortingdown() 
+    } 
+})
+
+function sortingup() {
+    let sorted = [];
+    for (let i = 0; i < items.length; i++) {
+        sorted.push(items[i]);
+    }
+    sorted.sort(function(a, b) {
+        if (a.textContent > b.textContent) {return 1;} // как записать элегантнее, в одну строчку с одинм условием?? 
+        if (a.textContent < b.textContent) {return -1;} // Второе не нужно, но без него не срабатывает
+    });
+    sorted.forEach(task => list.appendChild(task));
+    sorting.classList.replace('down', 'up');
+}
+
+function sortingdown () {
+    let sorted = [];
+    for (let i = 0; i < items.length; i++) {
+        sorted.push(items[i]);
+    }
+    sorted.sort(function(a, b) {
+        if (a.textContent > b.textContent) {return -1;} // как записать элегантнее, в одну строчку с одинм условием??
+        if (a.textContent < b.textContent) {return 1;}
+    });
+    sorted.forEach(task => list.appendChild(task));
+    sorting.classList.replace('up', 'down');
+}
